1ab3f541c96233ee726b4637112c22d3d4027544
renanbatistadias-ui <renan.batista.dias@escola.pr.gov.br>
2026-07-07 16:54:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-// Aguarda o carregamento do documento
-document.addEventListener("DOMContentLoaded", function() {
-    
-    // Seleciona o botão de "Descubra Como"
-    const botaoConhecer = document.getElementById("btn-conhecer");
-    
-    // Seleciona a seção para onde a página deve rolar
-    const secaoSobre = document.getElementById("sobre");
 
-    // Adiciona o evento de clique
-    botaoConhecer.addEventListener("click", function() {
-        secaoSobre.scrollIntoView({ 
-            behavior: "smooth" // Faz a rolagem ser suave
-        });
-    });
-});
